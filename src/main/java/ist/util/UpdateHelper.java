280f4defca07fab05d330b579c2255c9d92e8d24
wutai <1615140542@qq.com>
2020-11-03 16:26:27 +0800

    refactor it

--- src/main/java/ist/util/UpdateHelper.java
+++ src/main/java/ist/util/UpdateHelper.java
@@ -1,14 +1,30 @@
 package ist.util;
 
 import com.wps.api.tree.wps.Range;
+import com.wps.api.tree.wps.Style;
+
+import static ist.util.WpsApplication.currentActiveDoc;
 
 public class UpdateHelper {
 
-    public static void insertLineBreaks(Range range) {
+    public static void newLine(Range range) {
         range.InsertParagraphAfter();
     }
 
-    public static void newParagraph(Range range, String content) {
+    public static void supplement(Range range, String content) {
         range.InsertAfter(content);
+    }
+
+    public static int newParagraph(Range range, String content) {
+        String[] split = content.split("\r\n");
+        for (String s : split) {
+            supplement(range, s);
+            newLine(range);
+        }
+        return split.length;
+    }
+
+    public static void addDecoration(int start, int end, Style decoration) {
+        currentActiveDoc().Range(start, end).put_Style(decoration);
     }
 }
